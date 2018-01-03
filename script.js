a3eb901365983142a0c942c9b7cc7fdfdd3cb05a
bindiya02 <bindiyasonar1992@gmail.com>
2018-01-03 15:23:46 +0530

    js updated

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 920 || document.documentElement.scrollTop > 420) {
